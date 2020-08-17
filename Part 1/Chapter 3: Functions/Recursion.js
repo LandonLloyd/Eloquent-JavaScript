eeb27ff90f9ca1f117fd6ae86485745403277315
Landon Lloyd <65516863+LandonLloyd@users.noreply.github.com>
2020-08-16 21:14:07 -0600

    Create Recursion.js

---
+++ Part 1/Chapter 3: Functions/Recursion.js
@@ -0,0 +1,7 @@
+//Check if a number is even or not.
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
